d597ebfebf545ea04b4871c711fe05928b0b469e
Melziiie <rotasha.medford@gmail.com>
2019-07-17 17:45:14 +0800

    Menu

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <el-menu :default-active="activeLink" :router="true" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="/" :route="{name:'home'}">Home</el-menu-item>
+        <el-menu-item index="/about" :route="{name:'about'}">About</el-menu-item>
+      </el-menu>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.el-menu-item{
+  width:50%;
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
+
+<script>
+export default {
+  data() {
+      return {
+        activeLink: null,
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    },
+    watch:{
+      $route (to,from){
+        this.activeLink = to.path;
+      }
+    },
+    mounted: function(){
+      this.activeLink = this.$route.path;
+    }
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="about">
+    <el-row :gutter="8">
+      <el-col :span="12" v-for="(o) in 6" :key="o">
+        <el-card :body-style="{ padding: '0px' }">
+          <img src="https://picsum.photos/235/236" class="image">
+          <div style="padding: 10px;">
+            <span class="desc">Yummy hamburger</span>
+            <div class="bottom clearfix">
+              <span class="price">$40</span>
+              <el-button type="text" class="button">Read More..</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style>
+.el-row{
+  display:flex;
+  flex-wrap:wrap;
+  justify-content: center;
+}
+.el-col{
+  margin-top:20px;
+}
+  
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  } 
+  .price {
+    padding: 0;
+    float: left;
+  }
+  .button {
+    font-size:8px;
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+      display: table;
+      content: "";
+  }
+  
+  .clearfix:after {
+      clear: both
+  }
+</style>
+
+
